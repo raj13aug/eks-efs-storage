49443a87197f49702ba91e70c7b52a0c0ca2fd67
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-09-16 20:37:32 +0530

    Update requirements.tf

--- requirements.tf
+++ requirements.tf
@@ -1,14 +1,20 @@
 terraform {
+  required_version = "~> 1.0"
 
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
-    kubernetes = {
-      source = "hashicorp/kubernetes"
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.5.1"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14"
     }
   }
-  required_version = ">= 1.0.1"
 }
 
 locals {
