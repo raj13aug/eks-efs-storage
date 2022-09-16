fbb84c259164ef0c2568339c496bd878685a0669
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-09-16 18:09:08 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -9,7 +9,17 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
+
 provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  }
+}
+
+
+/* provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
@@ -33,4 +43,4 @@ provider "kubectl" {
     command     = "aws"
     args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
   }
-}
+} */
