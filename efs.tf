433ae03fc57ff9ad0d81dbdd39bd649ff1859c85
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-12-01 18:39:03 +0530

    updated

---
+++ efs.tf
@@ -0,0 +1,104 @@
+# add efs_csi_driver
+
+module "efs_csi_driver_irsa_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "4.20.1"
+
+  role_name             = "efs-csi-driver"
+  attach_efs_csi_policy = true
+
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:efs-csi-driver"]
+    }
+  }
+}
+
+resource "kubernetes_service_account" "efs_csi_driver" {
+  metadata {
+    name      = "efs-csi-driver"
+    namespace = "kube-system"
+    labels = {
+      "app.kubernetes.io/component" = "driver"
+      "app.kubernetes.io/name"      = "efs-csi-driver"
+    }
+    annotations = {
+      "eks.amazonaws.com/role-arn"               = module.efs_csi_driver_irsa_role[0].iam_role_arn
+      "eks.amazonaws.com/sts-regional-endpoints" = "true"
+    }
+  }
+}
+
+resource "helm_release" "efs_csi_driver" {
+  name       = "aws-efs-csi-driver"
+  chart      = "aws-efs-csi-driver"
+  repository = "https://kubernetes-sigs.github.io/aws-efs-csi-driver"
+  version    = "2.2.5"
+  namespace  = "kube-system"
+
+  set {
+    name  = "image.repository"
+    value = "602401143452.dkr.ecr.us-east-1.amazonaws.com/eks/aws-efs-csi-driver"
+  }
+  set {
+    name  = "controller.serviceAccount.create"
+    value = "false"
+  }
+  set {
+    name  = "controller.serviceAccount.name"
+    value = kubernetes_service_account.efs_csi_driver[0].metadata[0].name
+  }
+
+}
+
+
+resource "aws_efs_file_system" "efs" {
+  creation_token   = "efs-staging"
+  performance_mode = "generalPurpose"
+  throughput_mode  = "bursting"
+  encrypted        = "true"
+}
+
+
+resource "aws_security_group" "efs" {
+  name        = "efs-sg"
+  description = "Allows inbound EFS traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port       = 2049
+    to_port         = 2049
+    protocol        = "tcp"
+    security_groups = [module.eks.node_security_group_id]
+  }
+}
+
+resource "aws_security_group_rule" "efs" {
+  description              = "Allow outbound EFS traffic"
+  type                     = "egress"
+  from_port                = 2049
+  to_port                  = 2049
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.efs.id
+  security_group_id        = module.eks.node_security_group_id
+}
+
+# Availability zones
+data "aws_subnet" "private_subnet" {
+  for_each = toset(var.aws_private_subnets)
+  id       = each.key
+}
+
+locals {
+  subnet = { for subnet in data.aws_subnet.private_subnet : subnet.availability_zone => subnet.id }
+}
+
+
+resource "aws_efs_mount_target" "efs" {
+  for_each        = local.subnet
+  file_system_id  = aws_efs_file_system.efs.id
+  subnet_id       = each.value
+  security_groups = [aws_security_group.efs.id]
+}
+
