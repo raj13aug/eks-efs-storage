5704084397c61e2c66010fe38862a1d4a21d99b5
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-12-07 16:38:02 +0530

    update the role

--- efs-storage.tf
+++ efs-storage.tf
@@ -57,6 +57,21 @@ resource "helm_release" "efs_csi_driver" {
     value = kubernetes_service_account.efs_csi_driver.metadata.0.name
   }
 
+  set {
+    name  = "node.serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "node.serviceAccount.name"
+    value = "aws-efs-csi-driver-sa"
+  }
+
+  set {
+    name  = "node.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.eks_efs_driver_role.arn
+  }
+
   depends_on = [
     aws_efs_mount_target.efs_target
   ]
@@ -74,6 +89,92 @@ resource "aws_efs_file_system" "efs" {
 }
 
 
+
+###############################################################################
+# Create an IAM policy and role
+###############################################################################
+
+
+data "aws_iam_policy_document" "efs_csi_driver" {
+  statement {
+    actions = [
+      "elasticfilesystem:DescribeAccessPoints",
+      "elasticfilesystem:DescribeFileSystems",
+      "elasticfilesystem:DescribeMountTargets",
+      "ec2:DescribeAvailabilityZones"
+    ]
+    resources = ["*"]
+    effect    = "Allow"
+  }
+
+  statement {
+    actions = [
+      "elasticfilesystem:CreateAccessPoint"
+    ]
+    resources = ["*"]
+    effect    = "Allow"
+    condition {
+      test     = "StringLike"
+      variable = "aws:RequestTag/efs.csi.aws.com/cluster"
+      values   = ["true"]
+    }
+  }
+
+  statement {
+    actions = [
+      "elasticfilesystem:DeleteAccessPoint"
+    ]
+    resources = ["*"]
+    effect    = "Allow"
+    condition {
+      test     = "StringEquals"
+      variable = "aws:ResourceTag/efs.csi.aws.com/cluster"
+      values   = ["true"]
+    }
+  }
+}
+
+# create policy
+resource "aws_iam_policy" "eks_efs_driver_policy" {
+  name        = "xac-efs-csi-driver-policy"
+  description = "allow EKS access to EFS"
+  policy      = data.aws_iam_policy_document.efs_csi_driver.json
+}
+
+# create role
+resource "aws_iam_role" "eks_efs_driver_role" {
+  depends_on         = [module.eks]
+  name               = "xac-efs-csi-driver-role"
+  assume_role_policy = <<-EOF
+   {
+     "Version": "2012-10-17",
+     "Statement": [
+       {
+         "Effect": "Allow",
+         "Principal": {
+           "Federated": "${module.eks.oidc_provider_arn}"
+         },
+         "Action": "sts:AssumeRoleWithWebIdentity",
+         "Condition": {
+           "StringEquals": {
+             "oidc.eks.${local.region}.amazonaws.com/id/${basename(module.eks.oidc_provider_arn)}:sub": "system:serviceaccount:kube-system:aws-efs-csi-driver-sa"
+           }
+         }
+       }
+     ]
+   }
+   EOF
+}
+
+resource "aws_iam_policy_attachment" "eks_efs_driver_attach" {
+  name       = "eks_efs_driver_attach"
+  roles      = ["${aws_iam_role.eks_efs_driver_role.name}"]
+  policy_arn = aws_iam_policy.eks_efs_driver_policy.arn
+}
+
+
+
+#######################################
 # EFS Mount Targets
 /* resource "aws_efs_mount_target" "xac-airflow-efs-mt" {
   count           = length(data.aws_availability_zones.available.names)
